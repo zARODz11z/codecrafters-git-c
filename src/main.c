40bf1fbdc7df513c2bcc91e632d7f18b17f34885
Andrew Rodriguez <andrewrod003@gmail.com>
2024-12-24 16:22:09 -0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -22,22 +22,22 @@ int main(int argc, char *argv[]) {
 
         // Uncomment this block to pass the first stage
         // 
-        // if (mkdir(".git", 0755) == -1 || 
-        //     mkdir(".git/objects", 0755) == -1 || 
-        //     mkdir(".git/refs", 0755) == -1) {
-        //     fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
-        //     return 1;
-        // }
-        // 
-        // FILE *headFile = fopen(".git/HEAD", "w");
-        // if (headFile == NULL) {
-        //     fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
-        //     return 1;
-        // }
-        // fprintf(headFile, "ref: refs/heads/main\n");
-        // fclose(headFile);
-        // 
-        // printf("Initialized git directory\n");
+        if (mkdir(".git", 0755) == -1 || 
+            mkdir(".git/objects", 0755) == -1 || 
+            mkdir(".git/refs", 0755) == -1) {
+            fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
+            return 1;
+        }
+        
+        FILE *headFile = fopen(".git/HEAD", "w");
+        if (headFile == NULL) {
+            fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
+            return 1;
+        }
+        fprintf(headFile, "ref: refs/heads/main\n");
+        fclose(headFile);
+        
+        printf("Initialized git directory\n");
     } else {
         fprintf(stderr, "Unknown command %s\n", command);
         return 1;
